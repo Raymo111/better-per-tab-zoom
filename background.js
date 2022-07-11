7f8e135ceb4dcac19d16ef7dfda12ea3d832be23
Raymond Li <hi@raymond.li>
2022-07-10 21:10:56 -0400

    Try adding Ctrl+=/-

--- background.js
+++ background.js
@@ -1,10 +1,22 @@
-chrome.runtime.onMessage.addListener(function(msg, sender) {
-    if (msg.action == "zoom") {
+const zoomIncrement = 0.1;
+
+chrome.runtime.onMessage.addListener(function (msg, sender) {
+    if (msg.action === "zoom") { // only message right now
+
+        // If shift is pressed, change domain zoom
+        // Otherwise, change tab zoom
         chrome.tabs.setZoomSettings(sender.tab.id, {
             scope: msg.domain ? "per-origin" : "per-tab"
         });
+
+        console.log("zoom", msg.amount);
+
+        // Zoom zoom :)
         chrome.tabs.getZoom(sender.tab.id).then((curr_zoom) => {
-            chrome.tabs.setZoom(sender.tab.id, curr_zoom + (msg.amount > 0 ? -0.1 : 0.1))
+            chrome.tabs.setZoom(sender.tab.id, curr_zoom + (msg.amount > 0 ? -zoomIncrement : zoomIncrement))
         });
+    }
+    if (msg.error === "bad keyCode") {
+        console.log("bad keyCode", msg.shiftKey, msg.keyCode);
     }
 });
--- controller.js
+++ controller.js
@@ -1,6 +1,30 @@
+// Handle Ctrl+scroll
 document.addEventListener('wheel', function (e) {
-  if (!e.ctrlKey)
-    return;
-  e.preventDefault();
-  chrome.runtime.sendMessage({action: "zoom", domain: e.shiftKey, amount: e.deltaY});
+
+    // Do nothing if Ctrl key is not pressed
+    if (!e.ctrlKey) {
+        return;
+    }
+
+    // Prevent default behavior
+    e.preventDefault();
+
+    // Pass to background script
+    chrome.runtime.sendMessage({action: "zoom", domain: e.shiftKey, amount: e.deltaY});
 }, {passive: false});
+
+// Handle Ctrl+=/-
+document.addEventListener('keyDown', function (e) {
+
+    // Do nothing if Ctrl key is not pressed or key is not '=' or '-'
+    if (!e.ctrlKey || (e.keycode != 187 && e.keycode != 189)) {
+        chrome.runtime.sendMessage({error: "bad keyCode", shiftKey: e.shiftKey, keyCode: e.keyCode});
+        return;
+    }
+
+    // Prevent default behavior
+    e.preventDefault();
+
+    // Pass to background script
+    chrome.runtime.sendMessage({action: "zoom", domain: e.shiftKey, amount: e.keyCode == 187 ? 1 : -1});
+});
